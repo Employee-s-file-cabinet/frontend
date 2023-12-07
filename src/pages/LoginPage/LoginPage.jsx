701037d9e24fd2e0acf87b850bc8feb31e28df75
Андрей Гришков <andreygorm@gmail.com>
2023-12-08 00:26:33 +0300

    feat: add fonts

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Login from '../../components/Reusable/Login/Login';
-// import ReturnButton from '../../components/Reusable/ReturnButton/ReturnButton';
 
 import { MainButton } from '../../components/UI/Buttons/MainButton';
+import { Logo } from '../../components/Logo/Logo';
 
 export default function LoginPage() {
   const navigate = useNavigate();
@@ -13,10 +12,9 @@ export default function LoginPage() {
   };
 
   return (
-    <main>
-      <section className="login-page">
-        <h2 className="title is-4">СТРАНИЦА ЛОГИНА</h2>
-        <Login />
+    <section className="login-page">
+      <Logo />
+      <form>
         <MainButton
           label="На главную"
           disabled={false}
@@ -24,7 +22,11 @@ export default function LoginPage() {
           theme="is-link"
           onClick={handleClick}
         />
-      </section>
-    </main>
+      </form>
+      <footer>
+        <p>Политика конфиденциальности</p>
+        <p>© Все права защищены</p>
+      </footer>
+    </section>
   );
 }
