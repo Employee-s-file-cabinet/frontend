e085b4cb26a66b22ffea9edcd1a6fb021cbe3cfe
Denis Manaev <manaewd@yandex.ru>
2023-12-15 11:51:55 +0700

    tree created

--- src/pages/EmployeePage/EmployeePage.jsx
+++ src/pages/EmployeePage/EmployeePage.jsx
@@ -1,7 +1,10 @@
+import ProfileInfo from './ProfileInfo/ProfileInfo';
+
 export default function EmployeePage() {
   return (
     <section className="employee-page">
       <h2 className="title is-4">СТРАНИЦА КАРТОЧКИ СОТРУДНИКА</h2>
+      <ProfileInfo />
     </section>
   );
 }
