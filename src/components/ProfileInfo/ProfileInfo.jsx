bc4821ddcd6a18eaffc42d9f6c08682858473b1a
Stas Zaitsev <ststas@yandex.ru>
2023-12-22 13:00:05 +0200

    refactor(profile info): updated

--- src/components/ProfileInfo/ProfileInfo.jsx
+++ src/components/ProfileInfo/ProfileInfo.jsx
@@ -1,16 +1,14 @@
-/* eslint-disable no-unused-vars */
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 import { Icon } from '../UI/Icons/Icons';
 import ProfilePic from '../../assets/images/profile.jpg';
+import './ProfileInfo.scss';
 
 function ProfileInfo() {
   const [isEdit, setIsEdit] = useState(false);
   const {
     register,
-    handleSubmit,
-    resetField,
-    formState: { isValid, errors },
+    formState: { errors },
   } = useForm();
 
   function handleEditButton() {
@@ -19,221 +17,236 @@ function ProfileInfo() {
 
   return (
     <section className="profile">
-      <h2 className="title is-2">Исаева Полина Артёмовна</h2>
-      <div className="profile__content">
+      <h2 className="profile__title title is-2">Исаева Полина Артёмовна</h2>
+      <div className="profile__container">
         <div className="profile__image-container">
           <img className="profile__image" alt="Логотип" src={ProfilePic} />
-          <div className="file profile__image-file">
-            <label className="file-label" htmlFor="profileImage">
+          <div
+            className={`profile__file file ${
+              isEdit === false ? 'profile__buttons-disabled' : ''
+            }`}
+          >
+            <label className="profile__label file-label" htmlFor="profileImage">
               <input
                 {...register('file')}
                 className="file-input"
                 type="file"
                 name="profileImage"
               />
-
-              <span className="file-cta profile__image-cta">
-                <span className="file-label profile__file-label">
+              <span className="profile__cta file-cta">
+                <span className="profile__label file-label">
                   Загрузить фото
                 </span>
-                <span className="file-icon">
+                <span className="profile__icon file-icon">
                   <i className="fas fa-upload" />
                 </span>
               </span>
             </label>
-            <p className="error-text">{errors.email?.message}</p>
+            <span className="profile__input-error">
+              {errors.email?.message}
+            </span>
           </div>
         </div>
-
         <form className="profile__form" id="submit" name="profile">
-          <div className="columns is-gapless">
-            <div className="column is-one-third">
-              <div className="field">
-                <label className="label" htmlFor="lastName">
+          <div className="columns">
+            <div className="column profile__column">
+              <div className="profile__input">
+                <label className="profile__input-label" htmlFor="lastName">
                   Фамилия
-                  <div className="control profile__control">
-                    <input
-                      {...register('lastName')}
-                      className="input"
-                      type="text"
-                      placeholder="Фамилия"
-                      value="Исаева"
-                      disabled={!isEdit}
-                    />
-                  </div>
+                  <input
+                    {...register('lastName')}
+                    className="input profile__input"
+                    type="text"
+                    minLength="2"
+                    maxLength="150"
+                    placeholder="Фамилия"
+                    value="Исаева"
+                    disabled={!isEdit}
+                    required
+                  />
                 </label>
-                <p className="error">{errors.lastName?.message}</p>
+                <span className="profile__input-error">
+                  {errors.lastName?.message}
+                </span>
               </div>
-            </div>
-
-            <div className="column is-one-third">
-              <div className="field">
-                <label className="label" htmlFor="department">
-                  Отдел
-                  <div className="control profile__control">
-                    <input
-                      {...register('department')}
-                      className="input"
-                      type="text"
-                      placeholder="Отдел"
-                      value="Кадры"
-                      disabled={!isEdit}
-                    />
-                  </div>
-                </label>
-                <p className="error">{errors.department?.message}</p>
-              </div>
-            </div>
-            <div className="column is-one-third">
-              <div className="field">
-                <label className="label" htmlFor="mobileNumber">
-                  Мобильный телефон
-                  <div className="control profile__control">
-                    <input
-                      {...register('mobileNumber')}
-                      className="input"
-                      type="text"
-                      placeholder="Мобильный телефон"
-                      value="+79991000203"
-                      disabled={!isEdit}
-                    />
-                  </div>
-                </label>
-                <p className="error">{errors.mobileNumber?.message}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="columns is-gapless">
-            <div className="column is-one-third">
-              <div className="field">
-                <label className="label" htmlFor="firstName">
+              <div className="profile__input">
+                <label className="profile__input-label" htmlFor="firstName">
                   Имя
-                  <div className="control profile__control">
-                    <input
-                      {...register('firstName')}
-                      className="input"
-                      type="text"
-                      placeholder="Имя"
-                      value="Полина"
-                      disabled={!isEdit}
-                    />
-                  </div>
+                  <input
+                    {...register('firstName')}
+                    className="input profile__input"
+                    type="text"
+                    minLength="2"
+                    maxLength="150"
+                    placeholder="Имя"
+                    value="Полина"
+                    disabled={!isEdit}
+                    required
+                  />
                 </label>
-                <p className="error">{errors.firstName?.message}</p>
+                <span className="profile__input-error">
+                  {errors.firstName?.message}
+                </span>
               </div>
-            </div>
-            <div className="column is-one-third">
-              <div className="field">
-                <label className="label" htmlFor="jobTitle">
-                  Должность
-                  <div className="control profile__control">
-                    <input
-                      {...register('jobTitle')}
-                      className="input"
-                      type="text"
-                      placeholder="Должность"
-                      value="HR-менеджер"
-                      disabled={!isEdit}
-                    />
-                  </div>
-                </label>
-                <p className="error">{errors.jobTitle?.message}</p>
-              </div>
-            </div>
-            <div className="column is-one-third">
-              <div className="field">
-                <label className="label" htmlFor="extNumber">
-                  Внутренний номер
-                  <div className="control profile__control">
-                    <input
-                      {...register('extNumber')}
-                      className="input"
-                      type="text"
-                      placeholder="Внутренний номер"
-                      value="33-33"
-                      disabled={!isEdit}
-                    />
-                  </div>
-                </label>
-                <p className="error">{errors.extNumber?.message}</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="columns is-gapless">
-            <div className="column is-one-third">
-              <div className="field">
-                <label className="label" htmlFor="middleName">
+              <div className="profile__input">
+                <label className="profile__input-label" htmlFor="middleName">
                   Отчество
-                  <div className="control profile__control">
-                    <input
-                      {...register('middleName')}
-                      className="input"
-                      type="text"
-                      placeholder="Отчество"
-                      value="Артемовна"
-                      disabled={!isEdit}
-                    />
-                  </div>
+                  <input
+                    {...register('middleName')}
+                    className="input profile__input"
+                    type="text"
+                    minLength="2"
+                    maxLength="150"
+                    placeholder="Отчество"
+                    value="Артемовна"
+                    disabled={!isEdit}
+                    required
+                  />
                 </label>
-                <p className="error">{errors.middleName?.message}</p>
+                <span className="profile__input-error profile__input-error_margin_custom">
+                  {errors.middleName?.message}
+                </span>
               </div>
             </div>
-            <div className="column is-one-third">
-              <div className="field">
-                <label className="label" htmlFor="grade">
+            <div className="profile__column column">
+              <div className="profile__input">
+                <label className="profile__input-label" htmlFor="department">
+                  Отдел
+                  <input
+                    {...register('department')}
+                    className="input profile__input"
+                    type="text"
+                    minLength="2"
+                    maxLength="150"
+                    placeholder="Отдел"
+                    value="Кадры"
+                    disabled={!isEdit}
+                    required
+                  />
+                </label>
+                <span className="profile__input-error">
+                  {errors.department?.message}
+                </span>
+              </div>
+              <div className="profile__input">
+                <label className="profile__input-label" htmlFor="jobTitle">
+                  Должность
+                  <input
+                    {...register('jobTitle')}
+                    className="input profile__input"
+                    type="text"
+                    minLength="2"
+                    maxLength="150"
+                    placeholder="Должность"
+                    value="HR-менеджер"
+                    disabled={!isEdit}
+                    required
+                  />
+                </label>
+                <span className="profile__input-error">
+                  {errors.jobTitle?.message}
+                </span>
+              </div>
+              <div className="profile__input">
+                <label className="profile__input-label" htmlFor="grade">
                   Грейд
-                  <div className="control profile__control">
-                    <input
-                      {...register('grade')}
-                      className="input"
-                      type="number"
-                      placeholder="Грейд"
-                      value="4"
-                      disabled={!isEdit}
-                    />
-                  </div>
+                  <input
+                    {...register('grade')}
+                    className="input profile__input"
+                    type="text"
+                    minLength="2"
+                    maxLength="150"
+                    placeholder="Грейд"
+                    value="4"
+                    disabled={!isEdit}
+                    required
+                  />
                 </label>
-                <p className="error">{errors.grade?.message}</p>
+                <span className="profile__input-error profile__input-error_margin_custom">
+                  {errors.grade?.message}
+                </span>
               </div>
             </div>
-            <div className="column is-one-third">
-              <div className="field">
-                <label className="label" htmlFor="email">
-                  E-mail
-                  <div className="control profile__control">
-                    <input
-                      {...register('email')}
-                      className="input"
-                      type="email"
-                      placeholder="E-mail"
-                      value="IsaevaPA@company.com"
-                      disabled={!isEdit}
-                    />
-                  </div>
+            <div className="profile__column column">
+              <div className="profile__input">
+                <label className="profile__input-label" htmlFor="mobileNumber">
+                  Мобильный телефон
+                  <input
+                    {...register('mobileNumber')}
+                    className="input profile__input"
+                    type="text"
+                    minLength="2"
+                    maxLength="150"
+                    placeholder="Мобильный телефон"
+                    value="+79991000203"
+                    disabled={!isEdit}
+                    required
+                  />
                 </label>
-                <p className="error">{errors.email?.message}</p>
+                <span className="profile__input-error">
+                  {errors.mobileNumber?.message}
+                </span>
+              </div>
+              <div className="profile__input">
+                <label className="profile__input-label" htmlFor="extNumber">
+                  Внутренний номер
+                  <input
+                    {...register('extNumber')}
+                    className="input profile__input"
+                    type="text"
+                    minLength="2"
+                    maxLength="150"
+                    placeholder="Внутренний номер"
+                    value="33-33"
+                    disabled={!isEdit}
+                    required
+                  />
+                </label>
+                <span className="profile__input-error">
+                  {errors.extNumber?.message}
+                </span>
+              </div>
+              <div className="profile__input">
+                <label className="profile__input-label" htmlFor="email">
+                  E-mail
+                  <input
+                    {...register('email')}
+                    className="input profile__input"
+                    type="text"
+                    minLength="2"
+                    maxLength="150"
+                    placeholder="E-mail"
+                    value="IsaevaPA@company.com"
+                    disabled={!isEdit}
+                    required
+                  />
+                </label>
+                <span className="profile__input-error profile__input-error_margin_custom">
+                  {errors.email?.message}
+                </span>
               </div>
             </div>
           </div>
-
           <div
-            className={`buttons ${
+            className={`profile__buttons ${
               isEdit === false ? 'profile__buttons-disabled' : ''
             }`}
           >
-            <button className="button is-primary">Сохранить</button>
-            <button className="button is-primary is-light">
+            <button className="profile__button button is-primary">
+              Сохранить
+            </button>
+            <button className="profile__button button is-primary is-light">
               Сбросить изменения
             </button>
           </div>
         </form>
-        <Icon
-          icon="fa-pen-to-square"
-          size="is-small"
-          onClick={() => handleEditButton()}
-        />
+        <div className="profile__button-edit">
+          <Icon
+            icon="fa-pen-to-square"
+            size="is-small"
+            onClick={() => handleEditButton()}
+          />
+        </div>
       </div>
     </section>
   );
