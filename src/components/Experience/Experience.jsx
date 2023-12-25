d0af5ea2c9dba9b483d83b72877539d65d68ad5e
OLYAK\yaral <lyalyolik.k@yandex.ru>
2023-12-25 12:59:41 +0300

    finish

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,11 +1,8 @@
 import './Experience.scss';
 
-// import Trash from '../../assets/images/trash.png';
-
 export default function Experience({ isEdit }) {
   return (
     <>
-      {/* <div className="columns is-gapless career-development"> */}
       <div className="field">
         <legend className="label label-horizontal">Название компании</legend>
         <div className="control">
@@ -16,9 +13,6 @@ export default function Experience({ isEdit }) {
             disabled={!isEdit}
           />
         </div>
-        {/* <div className="columns is-gapless">
-          <img src={Trash} className='input-trash' alt="корзина" />
-        </div> */}
       </div>
       <div className="field">
         <legend className="label label-horizontal">Должность</legend>
@@ -30,9 +24,6 @@ export default function Experience({ isEdit }) {
             disabled={!isEdit}
           />
         </div>
-        {/* <div className="columns is-gapless">
-          <img src={Trash} className='input-trash' alt="корзина" />
-        </div> */}
       </div>
       <div className="columns is-gapless">
         <div className="column is-one-third">
@@ -64,14 +55,12 @@ export default function Experience({ isEdit }) {
           </div>
         </div>
       </div>
-
       <div className="field  ">
         <legend className="label label-horizontal">Функционал</legend>
         <div className="control">
           <textarea className="textarea is-small" disabled={!isEdit} />
         </div>
       </div>
-      {/* </div> */}
     </>
   );
 }
