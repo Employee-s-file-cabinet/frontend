75955b347d4de0336f15dede3c12cf167e55abee
Stas Zaitsev <ststas@yandex.ru>
2023-12-27 21:04:31 +0200

    fix(constants): update apiURL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const apiURL = 'https://ecabinet.acceleratorpracticum.ru:8001/api/v1';
+export const apiURL = 'http://127.0.0.1:9990';
 
 export const pagesPath = {
   home: { path: '', label: 'Главная', id: 1 },
