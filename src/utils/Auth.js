4004b1fb74e33c6be69d839a3aa5ba3d2f2217f2
Stas Zaitsev <ststas@yandex.ru>
2023-12-27 17:48:07 +0200

    fix(env variables): update variables

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const { REACT_APP_NODE_ENV, REACT_APP_PUBLIC_URL } = process.env;
+const { REACT_APP_NODE_ENV, REACT_APP_PUBLIC_URL, REACT_APP_DEV_URL } =
+  process.env;
 
 const baseURL =
   REACT_APP_NODE_ENV === 'production'
     ? REACT_APP_PUBLIC_URL
-    : 'http://localhost:4000';
+    : REACT_APP_DEV_URL;
 
 const axiosBase = axios.create({
   baseURL,
