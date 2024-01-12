90ee77c3f3292ad38cab23ea990cc4d601a183c0
Stas Zaitsev <ststas@yandex.ru>
2024-01-12 14:48:18 +0200

    feat(archive info validation): add PriorWork & Awards validation

--- src/utils/validation/EmployeeArcInfoValidation.js
+++ src/utils/validation/EmployeeArcInfoValidation.js
@@ -1,82 +1,78 @@
 import * as yup from 'yup';
 import { requiredFieldError } from '../constants';
 
-export const EmployeeProfileSchema = yup
-  .object({
-    lastName: yup
-      .string()
-      .required(requiredFieldError)
-      .matches(
-        /^(?=.{1,150}$)[а-яёА-ЯЁ]+(?:[-' ][а-яёА-ЯЁ]+)*$/,
-        'Используйте кириллицу, дефис и(или) апостроф.'
-      ),
-    firstName: yup
-      .string()
-      .required(requiredFieldError)
-      .matches(
-        /^(?=.{1,150}$)[а-яёА-ЯЁ]+(?:[-' ][а-яёА-ЯЁ]+)*$/,
-        'Используйте кириллицу, дефис и(или) апостроф.'
-      ),
-    middleName: yup
-      .string()
-      .required(requiredFieldError)
-      .matches(/^(?=.{1,150}$)[а-яёА-ЯЁ]*$/, 'Используйте кириллицу.'),
-    department: yup
-      .string()
-      .required(requiredFieldError)
-      .matches(
-        /^(?=.{2,150}$)[а-яёА-ЯЁ0-9]+(?:[-][а-яёА-ЯЁ0-9]+)*$/,
-        'Используте кириллицу, цифры, дефис.'
-      ),
-    jobTitle: yup
-      .string()
-      .required(requiredFieldError)
-      .matches(
-        /^(?=.{2,150}$)[а-яёА-ЯЁ0-9]+(?:[-][а-яёА-ЯЁ0-9]+)*$/,
-        'Используте кириллицу, цифры, дефис.'
-      ),
-    grade: yup
-      .string()
-      .required(requiredFieldError)
-      .matches(
-        /^(?=.{1,3}$)[0-9]*$/,
-        'Используте цифры. Макс. длина поля 3 символа.'
-      ),
-    mobileNumber: yup
-      .string()
-      .required(requiredFieldError)
-      .matches(
-        /^([+]{1,1})([0-9]{11,14})$/,
-        'Укажите телефон в формате "+79991110022". Длина номера от 11 до 14 цифр'
-      ),
-    extNumber: yup
-      .string()
-      .required(requiredFieldError)
-      .matches(
-        /^[0-9]{2,2}[-][0-9]{2,2}$/,
-        'Укажите телефон в формате "00-00".'
-      ),
-    email: yup
-      .string()
-      .required(requiredFieldError)
-      .matches(
-        /^\S+@\S+\.[a-zA-Z]{2,}$/,
-        'Введите корректный email. Пример: ru@ru.ru'
-      ),
+function formatDate(date) {
+  return new Date(date).toLocaleDateString().replaceAll('/', '.');
+}
 
-    picture: yup.lazy((value) => {
-      if (value) {
-        return value.length !== 0
-          ? yup
-              .mixed()
-              .test(
-                'fileSize',
-                'Размер файла превышает 5 Мб.',
-                (file) => file[0].size <= 5242880
-              )
-          : yup.mixed().notRequired();
-      }
-      return yup.mixed().notRequired();
-    }),
-  })
-  .required();
+export const EmployeeArcInfoValidationSchema = yup.object().shape({
+  priorWorkExperienceFieldSet: yup.array().of(
+    yup.object({
+      companyName: yup
+        .string()
+        .required(requiredFieldError)
+        .min(2, 'Минимальное кол-во символов 2')
+        .max(60, 'Максимальное кол-во символов 60'),
+      position: yup
+        .string()
+        .required(requiredFieldError)
+        .min(2, 'Минимальное кол-во символов 2')
+        .max(70, 'Максимальное кол-во символов 70'),
+      beginDate: yup
+        .date('Введите дату в формате ДД.ММ.ГГГГ')
+        .typeError('Введите дату в формате ДД.ММ.ГГГГ')
+        .required('Введите дату в формате ДД.ММ.ГГГГ')
+        .min(
+          'Jan 1 1900',
+          'Дата начала работы не должна быть раньше 01.01.1900'
+        )
+        .max(
+          Date(),
+          `Дата начала не должна быть позднее ${formatDate(Date())}`
+        ),
+
+      endDate: yup
+        .date()
+        .typeError('Введите дату в формате ДД.ММ.ГГГГ')
+        .when('beginDate', {
+          is: (value) => value.toString().includes('GMT'),
+          then: () =>
+            yup
+              .date()
+              .typeError('Введите дату в формате ДД.ММ.ГГГГ')
+              .required('Введите дату в формате ДД.ММ.ГГГГ')
+              .min(
+                yup.ref('beginDate'),
+                ({ min }) =>
+                  `Дата окончания не должна быть ранее ${formatDate(min)}`
+              ),
+        }),
+      jobDescription: yup
+        .string()
+        .notRequired()
+        .min(10, 'Минимальное кол-во символов 10')
+        .max(200, 'Максимальное кол-во символов 200'),
+    })
+  ),
+  awardsFieldSet: yup.array().of(
+    yup.object({
+      awardTitle: yup
+        .string()
+        .required(requiredFieldError)
+        .min(2, 'Минимальное кол-во символов 2')
+        .max(70, 'Максимальное кол-во символов 70'),
+      awardDate: yup
+        .date('Введите дату в формате ДД.ММ.ГГГГ')
+        .typeError('Введите дату в формате ДД.ММ.ГГГГ')
+        .required('Введите дату в формате ДД.ММ.ГГГГ')
+        .min(
+          'Jan 1 1900',
+          'Дата начала работы не должна быть раньше 01.01.1900'
+        )
+        .max(
+          Date(),
+          `Дата начала не должна быть позднее ${formatDate(Date())}`
+        ),
+    })
+  ),
+});
